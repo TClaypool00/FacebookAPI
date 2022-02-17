61c1d31e560610f14716f174053450aa247d8a12
claypooltyler00 <claypooltyler00@gmail.com>
2022-02-16 21:56:34 -0600

    GetAll, GetSingle, and Create methods all work

--- models/User.php
+++ models/User.php
@@ -23,4 +23,34 @@ class User {
 
         return $stmt;
     }
+    
+    public function get_single() {
+        $stmt = $this->conn->prepare("CALL getSingleUser('{$this->userId}')");
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->first_name = $row['FirstName'];
+        $this->last_name = $row['LastName'];
+        $this->email = $row['Email'];
+        $this->isAdmin = $row['IsAdmin'];
+    }
+
+    public function create() {
+        $stmt = $this->conn->prepare("CALL insertUser('{$this->first_name}', '{$this->last_name}', '{$this->email}', '{$this->password}', '{$this->isAdmin}');");
+        $this->first_name = htmlspecialchars(strip_tags($this->first_name));
+        $this->last_name = htmlspecialchars(strip_tags($this->last_name));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->isAdmin = htmlspecialchars(strip_tags($this->isAdmin));
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        printf('Error: %s \n', $stmt->error);
+
+        return false;
+    }
 }
